cb38e0a07d2a35b910c6d1e2406bc23b176969e6
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-26 17:19:40 +1000

    Add REST controller

---
+++ src/main/java/com/alexkozyura/tutorial/springboot/topic/TopicsController.java
@@ -0,0 +1,21 @@
+package com.alexkozyura.tutorial.springboot.topic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicsController {
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework","Spring Framework D"),
+                new Topic("java", "Core Java", "Java Description"),
+                new Topic("javascript", "JavaScript", "JavaScript Description")
+        );
+    }
+}
