8f21494925068ca5230a31e4ae2bd36d45c6976d
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-27 00:34:28 +1000

    Add post Topic endpoint

--- src/main/java/com/alexkozyura/tutorial/springboot/topic/TopicService.java
+++ src/main/java/com/alexkozyura/tutorial/springboot/topic/TopicService.java
@@ -2,17 +2,18 @@ package com.alexkozyura.tutorial.springboot.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
         new Topic("spring", "Spring Framework","Spring Framework D"),
         new Topic("java", "Core Java", "Java Description"),
         new Topic("javascript", "JavaScript", "JavaScript Description")
-    );
+    ));
 
     public List<Topic> getAllTopics() {
         return topics;
@@ -20,5 +21,9 @@ public class TopicService {
 
     public Topic getTopic(String id) {
         return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 }
