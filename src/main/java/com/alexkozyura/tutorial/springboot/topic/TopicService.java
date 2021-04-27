d7a738a754b367520a8824399a675219826a984b
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-27 16:53:49 +1000

    Refactor using Topic repository

--- src/main/java/com/alexkozyura/tutorial/springboot/topic/TopicService.java
+++ src/main/java/com/alexkozyura/tutorial/springboot/topic/TopicService.java
@@ -1,43 +1,38 @@
 package com.alexkozyura.tutorial.springboot.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = new ArrayList<>(Arrays.asList(
-        new Topic("spring", "Spring Framework","Spring Framework D"),
-        new Topic("java", "Core Java", "Java Description"),
-        new Topic("javascript", "JavaScript", "JavaScript Description")
-    ));
+    @Autowired
+    private TopicRepository topicRepository;
 
     public List<Topic> getAllTopics() {
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);
         return topics;
     }
 
-    public Topic getTopic(String id) {
-        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    public Optional<Topic> getTopic(String id) {
+        return topicRepository.findById(id);
     }
 
     public void addTopic(Topic topic) {
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(String id, Topic topic){
-        for (int i = 0; i < topics.size(); i++) {
-            Topic t = topics.get(i);
-            if (t.getId().equals(id)) {
-                topics.set(i, topic);
-                return;
-            }
-        }
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id) {
-        topics.removeIf(topic -> topic.getId().equals(id));
+        topicRepository.deleteById(id);
     }
 }
